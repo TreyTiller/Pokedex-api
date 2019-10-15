14738237a8b16b2dcb3f87d690a38b5bfbb7739b
Trey <treytiller@outlook.com>
2019-10-15 08:14:44 -0400

    Final commit before deployment. Security updated

--- server.js
+++ server.js
@@ -5,23 +5,22 @@ const cors = require('cors');
 const helmet = require('helmet');
 const POKEDEX = require('./pokedex.json')
 
-console.log(process.env.API_TOKEN);
-
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
 
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
-    console.log('validate bearer token middleware')
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({ error: 'Unauthorized request' })
-    }
-    next()
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
 })
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
@@ -31,27 +30,38 @@ app.get('/types', function handleGetTypes(req, res) {
 })
 
 app.get('/pokemon', function handleGetPokemon(req, res) {
-    let response = POKEDEX.pokemon;
-  
-    // filter our pokemon by name if name query param is present
-    if (req.query.name) {
-      response = response.filter(pokemon =>
-        // case insensitive searching
-        pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
-      )
-    }
-  
-    // filter our pokemon by type if type query param is present
-    if (req.query.type) {
-      response = response.filter(pokemon =>
-        pokemon.type.includes(req.query.type)
-      )
-    }
-  
-    res.json(response)
-  })
-const PORT=8000;
+  let response = POKEDEX.pokemon;
+
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+  }
+
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
+
+  res.json(response)
+})
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening at http://localhost:${PORT}`)
 })
